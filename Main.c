826fffc4ab5caff36330efa80c812260d8b3f504
mmyrberg <m.myrberg@outlook.com>
2023-04-20 09:15:51 +0200

    Add comments to UART files

--- Main.c
+++ Main.c
@@ -1,23 +1,10 @@
-#include "led.h"
+#include "UART.h"
 
-Led_Type led1;
-Led_Type led2;
-
-int main(void){
-
+int main(){ //huvudfunktion som kallar på UART-drivrutinen och testar dess funktion
   USART2_Init();
-	Led_ctor(&led1,RED,OFF);
-	Led_ctor(&led2,BLUE,ON);
-	
-//	Led_getState(&led1);
-//	Led_getState(&led2);
-//	
-//	Led_setState(&led1,ON);
-//	
-	
-	
-	
-	while(1){
-	
-	}
+  
+  test_setup();
+  
+  while(1){}
+  
 }
--- UART.h
+++ UART.h
@@ -1,12 +1,13 @@
-ifndef  __UART_H
+// Denna fil knyter samman UART.c och Main.c 
+ifndef  __UART_H 
 #define  __UART_H
 
 
-#include "stm32f4xx.h"                 
-#include <stdio.h>
+#include "stm32f4xx.h" // Hämtar källkoden och hänvisningar för arbete på STM-hårdvara. 
+#include <stdio.h> //  Inkluderar standard I/O för C
 
-void USART2_Init(void);
-void test_setup(void);
+void USART2_Init(void); // Kallar på och refererar till deklarationen 
+void test_setup(void); // 
 
 
-#endif
+#endif // förtydligar avslutningen av vår headerfil
